27dc2fb35fda9ff04259368f0ad5db61d5f6be81
Joselu <joseluissgz97@gmail.com>
2023-11-05 17:45:42 +0100

    orderComponent and togglecasePipe

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { BasicsPagesComponent } from './pages/basics-pages/basics-pages.component';
 import { NumbersPageComponent } from './pages/numbers-page/numbers-page.component';
+import { OrderComponent } from './pages/order/order.component';
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
 import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
@@ -12,6 +14,10 @@ import { ProductsRoutingModule } from './products-routing.module';
     BasicsPagesComponent,
     NumbersPageComponent,
     UncommonPageComponent,
+    OrderComponent,
+
+    //Pipes
+    ToggleCasePipe,
   ],
   imports: [CommonModule, ProductsRoutingModule, PrimeNgModule],
 })
